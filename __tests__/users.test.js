8f69fa34d24dc5bf7344ad998a02a14b78656684
Abdullahi Elmi <abdullahielmi147@gmail.com>
2022-11-30 11:02:03 -0800

    POST logs in a user

---
+++ __tests__/users.test.js
@@ -0,0 +1,33 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('users route', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  const mockUser = {
+    firstName: 'Test',
+    lastName: 'User',
+    email: 'test@example.com',
+    password: '12345',
+  };
+
+  it('POST /api/v1/users creates new user', async () => {
+    const res = await request(app).post('/api/v1/users').send(mockUser);
+    expect(res.status).toBe(200);
+    const { firstName, lastName, email } = mockUser;
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      firstName,
+      lastName,
+      email,
+    });
+  });
+  afterAll(() => {
+    pool.end();
+  });
+});
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,12 +1,39 @@
+
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users2;
 
-CREATE TABLE users (
+CREATE TABLE users2 (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
     email TEXT NOT NULL,
     password_hash TEXT NOT NULL
-)
+);
+
+DROP TABLE IF EXISTS secrets;
+
+CREATE TABLE secrets (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title TEXT NOT NULL,
+    description TEXT NOT NULL,
+    createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO secrets(title, description)
+
+VALUES
+('positive thoughts', 'repeat I can do this'),
+('good rest', 'exercise, eat well and hydrate'),
+('peace of mind', 'prayer')
+
+
+
+
+
+
+
+
+
+
 
