7a514b8a0a8433fe66b72ff7740cfc940a03540c
Abdullahi Elmi <abdullahielmi147@gmail.com>
2022-12-03 17:40:07 -0800

    POST /api/v1/users/sessions logs in exsisting user

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -2,27 +2,40 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
 
 describe('users route', () => {
   beforeEach(() => {
     return setup(pool);
   });
 
-  const mockUser = {
-    email: 'test@example.com',
-    password: '12345',
-  };
-
   it('POST /api/v1/users creates new user', async () => {
-    const res = await request(app).post('/api/v1/users').send(mockUser);
+    const res = await request(app)
+      .post('/api/v1/users')
+      .send({ email: 'test@example.com', password: '12345' });
     expect(res.status).toBe(200);
-    const { email } = mockUser;
 
     expect(res.body).toEqual({
       id: expect.any(String),
-      email,
+      email: 'test@example.com',
     });
   });
+  it('POST /api/v1/users/sessions should log in an existing user', async () => {
+    // use the User Service to create new user
+    const mockUser = {
+      email: 'test@example.com',
+      password: '12345',
+    };
+    await UserService.create(mockUser);
+    // log in that user
+    const resp = await request(app)
+      .post('/api/v1/users/sessions')
+      .send(mockUser);
+    // confirm that I get a 200
+    expect(resp.status).toBe(200);
+    expect(resp.body).toEqual({ message: 'Signed in successfully' });
+  });
+
   afterAll(() => {
     pool.end();
   });
